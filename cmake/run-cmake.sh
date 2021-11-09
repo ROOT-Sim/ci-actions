23b55f2e3452656b0384c46fbb86d1e95c5c1212
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2021-11-09 16:21:24 +0100

    Working on CMake build action
    Working on the CMake build action (getting input parameters)
    
    Signed-off-by: Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>

--- cmake/run-cmake.sh
+++ cmake/run-cmake.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 set -eu
 
-case $(uname) in
-  Linux | Darwin | Windows* | MINGW* | MSYS*)
-    echo $INPUT_SOURCE_DIR
-    echo "Build dir: $INPUT_BUILD_DIR"
-    echo "Build type: ${INPUT_BUILD_TYPE}"
-    echo "CC: ${INPUT_CC}"
-    echo "CXX: ${INPUT_CXX}"
-    echo "target: ${INPUT_TARGET}"
-    echo "Run tests: ${INPUT_RUN_TESTS}"
+function prepare() {
+  mkdir -p $INPUT_BUILD_DIR
+  cd $INPUT_BUILD_DIR
+  export CC=$INPUT_CC
+  export CXX=$INPUT_CXX
+  cmake -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE -DCMAKE_C_FLAGS="$INPUT_CFLAGS" -DCMAKE_CXX_FLAGS="$INPUT_CXXFLAGS" $INPUT_SOURCE_DIR
+}
 
-    mkdir -p $INPUT_BUILD_DIR
-    cd $INPUT_BUILD_DIR
-    export CC=$INPUT_CC
-    export CXX=$INPUT_CXX
-    cmake -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE -DCMAKE_C_FLAGS="$INPUT_CFLAGS" -DCMAKE_CXX_FLAGS="$INPUT_CXXFLAGS" $INPUT_SOURCE_DIR
+function test() {
+  if [ "$INPUT_RUN_TESTS" = "true" ]; then
+    ctest
+  fi
+}
+
+case $(uname) in
+  Linux | Darwin)
+    prepare
     cmake --build . --config $INPUT_BUILD_TYPE --target $INPUT_TARGET
 
-    if [ "$INPUT_RUN_TESTS" = "true" ]; then
-      ctest
+    test
+    ;;
+
+  Windows* | MINGW* | MSYS*)
+    prepare
+    target=$INPUT_BUILD_TYPE
+    if [ "$target" = "all" ]; then
+      target="all_build"
     fi
+    cmake --build . --config $target --target $INPUT_TARGET
+    test
     ;;
 
   *)
