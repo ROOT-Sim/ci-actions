56a2e1c904f2826b1f6aab67cb3e9016040f3ca0
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2021-11-09 21:57:14 +0100

    Working on docs generation
    Working on the docs generation action
    
    Signed-off-by: Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>

--- init/setup-doxygen.sh
+++ init/setup-doxygen.sh
@@ -4,15 +4,17 @@ set -eu
 case $(uname) in
   Linux)
    sudo apt-get update
-   sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq --no-install-recommends doxygen
+   sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq --no-install-recommends doxygen graphviz
    ;;
 
   Darwin)
     brew install doxygen
+    brew install graphviz
     ;;
 
   Windows* | MINGW* | MSYS*)
     choco install doxygen.install --installargs 'ADD_CMAKE_TO_PATH=System'
+    choco install graphviz --installargs 'ADD_CMAKE_TO_PATH=System'
     ;;
 
   *)
