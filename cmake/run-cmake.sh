31cbc33b80203ffc2a8163b33f2dd35f799874d9
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2021-11-15 13:18:43 +0100

    Specify Unix Makefiles as the target
    Possibly this fixes windows builds.
    
    Signed-off-by: Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>

--- cmake/run-cmake.sh
+++ cmake/run-cmake.sh
@@ -6,7 +6,7 @@ set -eu
 function prepare() {
   mkdir -p $INPUT_BUILD_DIR
   cd $INPUT_BUILD_DIR
-  cmake -DCMAKE_C_COMPILER=$INPUT_CC -DCMAKE_CXX_COMPILER=$INPUT_CXX -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE -DCMAKE_C_FLAGS="$INPUT_CFLAGS" -DCMAKE_CXX_FLAGS="$INPUT_CXXFLAGS" $INPUT_SOURCE_DIR
+  cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=$INPUT_CC -DCMAKE_CXX_COMPILER=$INPUT_CXX -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE -DCMAKE_C_FLAGS="$INPUT_CFLAGS" -DCMAKE_CXX_FLAGS="$INPUT_CXXFLAGS" $INPUT_SOURCE_DIR
 }
 
 function test() {
@@ -19,7 +19,7 @@ case $(uname) in
   Linux | Darwin)
     prepare
     cmake --build . --config $INPUT_BUILD_TYPE --target $INPUT_TARGET
-    test || cat /Users/runner/work/ci-actions/build/Testing/Temporary/LastTest.log
+    test
     ;;
 
   Windows* | MINGW* | MSYS*)
@@ -29,7 +29,7 @@ case $(uname) in
       target="ALL_BUILD"
     fi
     cmake --build . --config $INPUT_BUILD_TYPE --target $target
-    test || cat D:/a/ci-actions/build/Testing/Temporary/LastTest.log
+    test
     ;;
 
   *)
