4e69742214ad1ec190e8643920a329335f922e79
Andrea Piccione <piccione@diag.uniroma1.it>
2022-08-16 20:21:18 +0200

    Yet another cmke debug flag
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- cmake/run-cmake.sh
+++ cmake/run-cmake.sh
@@ -15,7 +15,6 @@ fi
 case $(uname) in
   Windows* | MINGW* | MSYS*)
     export MPI_HOME="/c/Program Files (x86)/Intel/oneAPI/mpi"
-    export VERBOSE=1
     ;;
 
   *)
@@ -26,7 +25,7 @@ case $(uname) in
   Linux | Darwin | Windows* | MINGW* | MSYS*)
     mkdir -p "$INPUT_BUILD_DIR"
     cd "$INPUT_BUILD_DIR"
-    cmake --debug-output -G "Unix Makefiles" -DCMAKE_C_COMPILER=$INPUT_CC -DCMAKE_CXX_COMPILER=$INPUT_CXX -DCMAKE_BUILD_TYPE="$INPUT_BUILD_TYPE" -DCMAKE_C_FLAGS="$INPUT_CFLAGS" -DCMAKE_CXX_FLAGS="$INPUT_CXXFLAGS" "$INPUT_SOURCE_DIR"
+    cmake --verbose=1 -G "Unix Makefiles" -DCMAKE_C_COMPILER=$INPUT_CC -DCMAKE_CXX_COMPILER=$INPUT_CXX -DCMAKE_BUILD_TYPE="$INPUT_BUILD_TYPE" -DCMAKE_C_FLAGS="$INPUT_CFLAGS" -DCMAKE_CXX_FLAGS="$INPUT_CXXFLAGS" "$INPUT_SOURCE_DIR"
     cmake --build . --config "$INPUT_BUILD_TYPE" --target "$INPUT_TARGET"
     if [ "$INPUT_RUN_TESTS" = "true" ]; then
       ctest --rerun-failed --output-on-failure
