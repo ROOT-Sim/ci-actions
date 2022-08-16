f33bd429a65028bba283a350ed650ca8bf158f91
Andrea Piccione <piccione@diag.uniroma1.it>
2022-08-16 21:00:14 +0200

    yet another tentative fix
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- cmake/run-cmake.sh
+++ cmake/run-cmake.sh
@@ -15,7 +15,7 @@ fi
 case $(uname) in
   Windows* | MINGW* | MSYS*)
     export I_MPI_ROOT="c:/Program Files (x86)/Intel/oneAPI/mpi"
-    export MPIEXEC_EXECUTABLE="c:/Program Files (x86)/Intel/oneAPI/mpi/bin/mpiexec.exe"
+    export MPIEXEC_PATH="c:/Program Files (x86)/Intel/oneAPI/mpi/bin/mpiexec.exe"
     ;;
 
   *)
@@ -26,7 +26,7 @@ case $(uname) in
   Linux | Darwin | Windows* | MINGW* | MSYS*)
     mkdir -p "$INPUT_BUILD_DIR"
     cd "$INPUT_BUILD_DIR"
-    cmake --trace-expand -G "Unix Makefiles" -DCMAKE_C_COMPILER=$INPUT_CC -DCMAKE_CXX_COMPILER=$INPUT_CXX -DCMAKE_BUILD_TYPE="$INPUT_BUILD_TYPE" -DCMAKE_C_FLAGS="$INPUT_CFLAGS" -DCMAKE_CXX_FLAGS="$INPUT_CXXFLAGS" "$INPUT_SOURCE_DIR"
+    cmake --trace-expand -G "Unix Makefiles" -DMPIEXEC_EXECUTABLE="$MPIEXEC_PATH" -DCMAKE_C_COMPILER=$INPUT_CC -DCMAKE_CXX_COMPILER=$INPUT_CXX -DCMAKE_BUILD_TYPE="$INPUT_BUILD_TYPE" -DCMAKE_C_FLAGS="$INPUT_CFLAGS" -DCMAKE_CXX_FLAGS="$INPUT_CXXFLAGS" "$INPUT_SOURCE_DIR"
     cmake --build . --config "$INPUT_BUILD_TYPE" --target "$INPUT_TARGET"
     if [ "$INPUT_RUN_TESTS" = "true" ]; then
       ctest --rerun-failed --output-on-failure
