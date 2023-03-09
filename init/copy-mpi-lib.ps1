b4ba32aad159ba52bcc3f1d0e413cd3e3972116b
Andrea Piccione <piccione@diag.uniroma1.it>
2023-03-09 09:45:09 +0100

    Fixed the mpi lib copy
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- init/copy-mpi-lib.ps1
+++ init/copy-mpi-lib.ps1
@@ -1,3 +1,3 @@
 # SPDX-FileCopyrightText: 2008-2021 HPDCS Group <rootsim@googlegroups.com>
 # SPDX-License-Identifier: GPL-3.0-only
-cp "$Env:I_MPI_ROOT\bin\release\impi.dll" "$Env:SystemRoot\System32"
+cp "C:\Program Files (x86)\Intel\oneAPI\mpi\latest\bin\release\impi.dll" "$Env:SystemRoot\System32"
