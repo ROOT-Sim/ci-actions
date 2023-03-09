dbf0684c87daafac929cfb1f5efb3571faaa7091
Andrea Piccione <piccione@diag.uniroma1.it>
2023-03-09 05:14:23 +0100

    Intel MPI from the officila installer
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- init/setup-mpi.sh
+++ init/setup-mpi.sh
@@ -13,9 +13,18 @@ case $(uname) in
     ;;
 
   Windows* | MINGW* | MSYS*)
-    pip install dpcpp-cpp-rt
-    pip install impi-devel
-    pip install impi_rt
+    installer_name=w_mpi_oneapi_p_2021.8.0.25543_offline.exe
+    base_url=https://registrationcenter-download.intel.com/akdlm/irc_nas/19160
+    curl -sO $base_url/$installer_name
+    ./$installer_name -s -a --silent --eula accept
+    ONEAPI_ROOT="C:\Program Files (x86)\Intel\oneAPI"
+    I_MPI_ROOT="${ONEAPI_ROOT}\mpi\latest"
+    echo "ONEAPI_ROOT=${ONEAPI_ROOT}" >> $GITHUB_ENV
+    echo "I_MPI_ROOT=${I_MPI_ROOT}" >> $GITHUB_ENV
+    echo "${I_MPI_ROOT}\bin" >> $GITHUB_PATH
+    echo "${I_MPI_ROOT}\bin\release" >> $GITHUB_PATH
+    echo "${I_MPI_ROOT}\libfabric\bin" >> $GITHUB_PATH
+    echo "${I_MPI_ROOT}\libfabric\bin\utils" >> $GITHUB_PATH
     ;;
 
   *)
