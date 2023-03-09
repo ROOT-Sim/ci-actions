64bbc5b12e2eb67c7699566eb6d6dbde60776fe3
Andrea Piccione <piccione@diag.uniroma1.it>
2023-03-09 09:41:02 +0100

    Getting there
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- init/copy-mpi-lib.ps1
+++ init/copy-mpi-lib.ps1
@@ -1,1 +1,3 @@
+# SPDX-FileCopyrightText: 2008-2021 HPDCS Group <rootsim@googlegroups.com>
+# SPDX-License-Identifier: GPL-3.0-only
 cp "$Env:I_MPI_ROOT\bin\release\impi.dll" "$Env:SystemRoot\System32"
