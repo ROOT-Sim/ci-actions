3d0f22d2170351c59134eecf108d2cd4eb7d3813
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2026-06-16 18:45:29 +0200

    Fix SPDX aggressive search by REUSE linter
    The replacement string is now broken in a multi
    string and the linter is happy.
    
    Incidentally, I have updated the name of the research
    group after the change.

--- docs/doxygen_spdx_filter.py
+++ docs/doxygen_spdx_filter.py
@@ -9,9 +9,9 @@ website = "https://hpdcs.github.io"
 with open(sys.argv[1], 'r') as f:
     file_text = f.read()
 
-file_text = re.sub(r"SPDX-FileCopyrightText: 2008-([0-9]{4}) HPCS Group .*>",
+file_text = re.sub(r"SPDX-FileCopyright" r"Text: 2008-([0-9]{4}) HPCS Group .*>",
                    r"@copyright Copyright (C) 2008-\1 HPCS Group " + website, file_text)
-file_text = re.sub(r"SPDX-License-Identifier:.*", "", file_text)
-file_text = re.sub(r"SPDX-FileCopyrightText:", "@copyright", file_text)
+file_text = re.sub(r"SPDX-License-" r"Identifier:.*", "", file_text)
+file_text = re.sub(r"SPDX-FileCopyright" r"Text:", "@copyright", file_text)
 
 print(file_text)
